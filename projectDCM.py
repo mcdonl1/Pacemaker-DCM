5be1deeda21bc9d299d8d1e2761e49f295254c07
mcdonl1 <lucmcd10@gmail.com>
2018-10-04 16:32:13 -0400

    Added authentication file, and worked on GUI front end

--- projectDCM.py
+++ projectDCM.py
@@ -1,11 +1,39 @@
 from tkinter import *
 
+#Initialize window
 window = Tk()
 window.title("Pacemaker DCM")
 
 lbl = Label(window, text="Sign in or create a new user account.")
-lbl.grid(column=0, row=0)
+lbl.grid(column=1, row=0)
 
-window.geometry('350x200')
+window.geometry('700x500')
+
+#Screen placeholders
+screenPlaceholder = Label(window, text="Create Account Screen")
+screenPlaceholder.grid(column=1, row=3)
+
+showCreateAcctScreen = True #If false, show login screen
+
+
+#Select screen
+def showCreateAcct():
+    screenPlaceholder.configure(text="Create Account Screen")
+    showCreateAcctScreen = True
+
+def showLogin():
+    screenPlaceholder.configure(text="Login Screen")
+    showCreateAcctScreen = False
+
+createAcctBtn = Button(window, text="Create Account", command=showCreateAcct)
+loginBtn = Button(window, text="Login", command=showLogin)
+createAcctBtn.grid(column=0, row=1)
+loginBtn.grid(column=1, row=1)
+
+#Create
+
+
+
 #open the window and await user interaction
 window.mainloop()
+
