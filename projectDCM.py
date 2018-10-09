1554231a6f7744e7deda255454d79d31b9a6cd2a
mcdonl1 <lucmcd10@gmail.com>
2018-10-09 10:32:45 -0400

    Changed stucture of GUI code to include useof class structures

--- projectDCM.py
+++ projectDCM.py
@@ -1,64 +1,112 @@
 from tkinter import *
 
-#Initialize window
-window = Tk()
-window.title("Pacemaker DCM")
+lineheight = 27
 
-lbl = Label(window, text="Sign in or create a new user account.")
-lbl.grid(column=1, row=0)
+#Initialize Window class
+class Window(Frame):
 
-window.geometry('700x500')
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+
+        #set title of master
+        self.master.title("Pacemaker DCM")
+
+        #allow widget to take full space of root window
+        self.pack(fill=BOTH, expand=1)
 
 
-showCreateAcctScreen = True #If false, show login screen
+        lbl = Label(root, text="Sign in or create a new user account.")
+        lbl.place(x=0, y=0)
+
+        def showLogin():
+            print("Show Login")
+            #show login screen
+
+        def showCreateAcct():
+            print("Show create account")
+            #show create account screen
+
+        createAcctScreenBtn = Button(root, text="Create Account", command=showCreateAcct)
+        loginScreenBtn = Button(root, text="Login", command=showLogin)
+
+        createAcctScreenBtn.place(x=0,y=1*lineheight)
+        loginScreenBtn.place(x=120,y=1*lineheight)
+
+        usernameLbl = Label(root, text="Username:")
+        pwLbl = Label(root, text="Password:")
+        cpwLbl = Label(root, text="Confirm Password:")
+        usernameField = Entry(root, width=20)
+        pwField = Entry(root, width=20)
+        cpwField = Entry(root, width=20)
+        createAccountBtn = Button(root, text="Create Account")
+
+        usernameLbl.place(x=0,y=2*lineheight)
+        pwLbl.place(x=0,y=3*lineheight)
+        cpwLbl.place(x=0,y=4*lineheight)
+        usernameField.place(x=120,y=2*lineheight)
+        pwField.place(x=120,y=3*lineheight)
+        cpwField.place(x=120,y=4*lineheight)
+        createAccountBtn.place(x=120,y=5*lineheight)
+
+root = Tk()
+app = Window(root)
+
+root.geometry('700x500')
 
 
-#Select screen
-def showCreateAcct():
-    showCreateAcctScreen = True
+# showCreateAcctScreen = True #If false, show login screen
 
-def showLogin():
-    showCreateAcctScreen = False
 
-createAcctScreenBtn = Button(window, text="Create Account", command=showCreateAcct)
-loginScreenBtn = Button(window, text="Login", command=showLogin)
-createAcctScreenBtn.grid(column=0, row=1)
-loginScreenBtn.grid(column=1, row=1)
+# #Select screen
+# def showCreateAcct():
+#     showCreateAcctScreen = True
 
-#Create Account Screen
-if showCreateAcctScreen:
-    #components
-    usernameLbl = Label(window, text="Username:")
-    pwLbl = Label(window, text="Password:")
-    cpwLbl = Label(window, text="Confirm Password:")
-    usernameField = Entry(window, width=20)
-    pwField = Entry(window, width=20)
-    cpwField = Entry(window, width=20)
-    createAccountBtn = Button(window, text="Create Account")
+# def showLogin():
+#     showCreateAcctScreen = False
 
-    #design
-    usernameLbl.grid(column=0,row=3)
-    pwLbl.grid(column=0,row=4)
-    cpwLbl.grid(column=0,row=5)
-    usernameField.grid(column=1,row=3)
-    pwField.grid(column=1,row=4)
-    cpwField.grid(column=1, row=5)
-    createAccountBtn.grid(column=1, row=6)
-else:
-    #components
-    usernameLbl = Label(window, text="Username:")
-    pwLbl = Label(window, text="Password:")
-    usernameField = Entry(window, width=20)
-    pwField = Entry(window, width=20)
-    loginBtn = Button(window, text="Login")
+# createAcctScreenBtn = Button(root, text="Create Account", command=showCreateAcct)
+# loginScreenBtn = Button(root, text="Login", command=showLogin)
+# createAcctScreenBtn.grid(column=0, row=1)
+# loginScreenBtn.grid(column=1, row=1)
 
-    #design
-    usernameLbl.grid(column=0,row=3)
-    pwLbl.grid(column=0,row=4)
-    usernameField.grid(column=1,row=3)
-    pwField.grid(column=1,row=4)
-    loginBtn.grid(column=1, row=5)
+# #Create Account Screen
+# if showCreateAcctScreen:
+#     #components
+#     usernameLbl = Label(root, text="Username:")
+#     pwLbl = Label(root, text="Password:")
+#     cpwLbl = Label(root, text="Confirm Password:")
+#     usernameField = Entry(root, width=20)
+#     pwField = Entry(root, width=20)
+#     cpwField = Entry(root, width=20)
+#     createAccountBtn = Button(root, text="Create Account")
 
-#open the window and await user interaction
-window.mainloop()
+#     #design
+#     usernameLbl.grid(column=0,row=3)
+#     pwLbl.grid(column=0,row=4)
+#     cpwLbl.grid(column=0,row=5)
+#     usernameField.grid(column=1,row=3)
+#     pwField.grid(column=1,row=4)
+#     cpwField.grid(column=1, row=5)
+#     createAccountBtn.grid(column=1, row=6)
+# else:
+#     #components
+#     usernameLbl = Label(root, text="Username:")
+#     pwLbl = Label(root, text="Password:")
+#     usernameField = Entry(root, width=20)
+#     pwField = Entry(root, width=20)
+#     loginBtn = Button(root, text="Login")
+
+#     #design
+#     usernameLbl.grid(column=0,row=3)
+#     pwLbl.grid(column=0,row=4)
+#     usernameField.grid(column=1,row=3)
+#     pwField.grid(column=1,row=4)
+#     loginBtn.grid(column=1, row=5)
+
+#open the root and await user interaction
+root.mainloop()
 
