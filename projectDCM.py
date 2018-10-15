718b2341fd9918606e765405d97a92d2807096b5
mcdonl1 <lucmcd10@gmail.com>
2018-10-14 20:46:59 -0400

    Deleted commented out block

--- projectDCM.py
+++ projectDCM.py
@@ -94,56 +94,6 @@ app.title("Pacemaker DCM")
 
 app.geometry('700x500')
 
-
-# showCreateAcctScreen = True #If false, show login screen
-
-
-# #Select screen
-# def showCreateAcct():
-#     showCreateAcctScreen = True
-
-# def showLogin():
-#     showCreateAcctScreen = False
-
-# createAcctScreenBtn = Button(self, text="Create Account", command=showCreateAcct)
-# loginScreenBtn = Button(self, text="Login", command=showLogin)
-# createAcctScreenBtn.grid(column=0, row=1)
-# loginScreenBtn.grid(column=1, row=1)
-
-# #Create Account Screen
-# if showCreateAcctScreen:
-#     #components
-#     usernameLbl = Label(self, text="Username:")
-#     pwLbl = Label(self, text="Password:")
-#     cpwLbl = Label(self, text="Confirm Password:")
-#     usernameField = Entry(self, width=20)
-#     pwField = Entry(self, width=20)
-#     cpwField = Entry(self, width=20)
-#     createAccountBtn = Button(self, text="Create Account")
-
-#     #design
-#     usernameLbl.grid(column=0,row=3)
-#     pwLbl.grid(column=0,row=4)
-#     cpwLbl.grid(column=0,row=5)
-#     usernameField.grid(column=1,row=3)
-#     pwField.grid(column=1,row=4)
-#     cpwField.grid(column=1, row=5)
-#     createAccountBtn.grid(column=1, row=6)
-# else:
-#     #components
-#     usernameLbl = Label(self, text="Username:")
-#     pwLbl = Label(self, text="Password:")
-#     usernameField = Entry(self, width=20)
-#     pwField = Entry(self, width=20)
-#     loginBtn = Button(self, text="Login")
-
-#     #design
-#     usernameLbl.grid(column=0,row=3)
-#     pwLbl.grid(column=0,row=4)
-#     usernameField.grid(column=1,row=3)
-#     pwField.grid(column=1,row=4)
-#     loginBtn.grid(column=1, row=5)
-
 #open the app and await user interaction
 app.mainloop()
 
